68ffad366311fa343058128e11b51c878c84f8f2
Louis Diaz <le.diaz@icloud.com>
2022-11-28 16:40:58 -0600

    reworking the page

--- scripts/figure.js
+++ scripts/figure.js
@@ -7,7 +7,7 @@ const galleryPhoto = 'gallery__photo';
 export default function makeFigure() {
     const figure = document.createElement('figure');
     figure.classList.add(galleryFigure);
-    figure.setAttribute('data-aos', 'fade-up');
+    // figure.setAttribute('data-aos', 'fade-up');
 
     const image = document.createElement('img');
     image.classList.add(galleryPhoto);
